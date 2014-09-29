650c12fd41dc686caea2c21ba412dfac24f40228
umbrae <umbrae@reddit.com>
2014-09-29 15:09:49 -0700

    Checkpoint

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,17 +1,35 @@
-  jQuery(function($) {
-    var panelList = $('#draggablePanelList');
+$(function() {
+  var panelList = $('#draggablePanelList');
 
-    panelList.sortable({
-        // Only make the .panel-heading child elements support dragging.
-        // Omit this to make then entire <li>...</li> draggable.
-        handle: '.panel-heading', 
-        update: function() {
-            $('.panel', panelList).each(function(index, elem) {
-                 var $listItem = $(elem),
-                     newIndex = $listItem.index();
+  // hax to get sorting to work properly
+  var maxHeight = Math.max.apply(null, panelList.find('li').map(function() { return $(this).height(); }))
+  panelList.find('li').height(maxHeight);
 
-                 // Persist the new indices.
-            });
-        }
+  if (localStorage.getItem('panel-sorting')) {
+    var sorting = JSON.parse(localStorage.getItem('panel-sorting'));
+    var newList = [];
+
+    $(sorting).each(function(i, el_id) {
+      newList.push(panelList.find('#' + el_id).detach())
     });
+    newList.push(panelList.find('li').detach())
+
+    panelList.append(newList)
+  }
+
+  panelList.sortable({
+      handle: '.panel-heading', 
+      update: function() {
+          $('.panel', panelList).each(function(index, elem) {
+               var $listItem = $(elem),
+                   newIndex = $listItem.index();
+
+               // Persist the new indices.
+          });
+
+          var panel_ids = panelList.children('li').map(function() { return this.id; }).toArray();
+          localStorage.setItem('panel-sorting', JSON.stringify(panel_ids));
+      }
+  });
+
 });
